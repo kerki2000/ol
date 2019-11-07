797e24647ddc55834efc51c0278a01fde3b3b4f3
Oleg <kerki2000@gmail.com>
2019-11-06 22:47:55 -0500

    ass1

---
+++ Ass1/Ass1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ass1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a program  to print all unique elements in an array
+            int[] num = { 2, 2, 2, 5, 8, 9, 6, 4, 4, 8, 7, 6, 6 };
+            UniqueElements(num);
+        }
+        public static void UniqueElements(int[] numbers)
+        {
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int count = 0;
+                for (int j = 0; j < numbers.Length; j++)
+                {
+                    if (numbers[i] == numbers[j])
+                        count++;
+
+                }
+                if (count < 2)
+                    Console.Write(numbers[i]+" ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
